e57fcc301504f4031c8a192202bf079d6b0fcb3b
mar20092 <mar20092@byui.edu>
2022-01-29 01:43:20 -0300

    Lesson 3 correction

--- lesson3z/js/script.js
+++
@@ -1,9 +0,0 @@
-var lastUpdated = document.lastModified;
-document.getElementById("date").innerHTML = lastUpdated;
-
-/*var y = new Date();
-var fullYear = y.getFullYear();
-document.getElementById("year").innerHTML = fullYear;*/
-
-var y = new Date();
-document.querySelector("#year").innerText = y.getFullYear();
